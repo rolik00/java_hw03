dca2466158a42c60d1370aec151ece8099817756
rolik00 <148611487+rolik00@users.noreply.github.com>
2024-03-16 21:09:08 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.time.DateTimeException;
 import java.util.*;
 import java.time.LocalDate;
 import java.time.Period;
@@ -10,15 +11,22 @@ public class Main {
         return "мужской";
     }
     private static int full_age(String date)
-    {   
+    {
         String[] date_parts = date.split("\\.");
         int d = Integer.parseInt(date_parts[0]);
         int m = Integer.parseInt(date_parts[1]);
         int y = Integer.parseInt(date_parts[2]);
         LocalDate current_date = LocalDate.now();
-        LocalDate brith_date = LocalDate.of(y, m, d);
-        Period difference = Period.between(brith_date, current_date);
-        int year = difference.getYears();
+        int year = -1;
+        try
+        {
+            LocalDate brith_date = LocalDate.of(y, m, d);
+            Period difference = Period.between(brith_date, current_date);
+            year = difference.getYears();
+        }
+        catch (DateTimeException ex) {
+            System.out.println("Неверно введена дата! (Такой даты не существует)");
+        }
         return year;
     }
     private static void print(String info)
@@ -26,6 +34,7 @@ public class Main {
         String[] parts = info.split(" ");
         String gender = get_gender(parts[2]);
         int age = full_age(parts[3]);
+        if (age == -1) return;
         System.out.println("ФИО: " + parts[0] + " " + parts[1].charAt(0) + parts[2].charAt(0));
         System.out.println("Пол: " + gender);
         System.out.println("Полный возраст: " + age + " лет");
